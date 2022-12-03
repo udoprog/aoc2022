a25c75e2462120c1c3f2c7493221c4ca0c7b50f7
John-John Tedro <udoprog@tedro.se>
2022-12-03 11:15:14 +0100

    Clean up documentation

--- years/2022/src/bin/d01.rs
+++ years/2022/src/bin/d01.rs
@@ -1,37 +1,30 @@
 use lib::prelude::*;
 
 fn main() -> Result<()> {
-    let mut totals = parse()?;
-    totals.sort();
-
-    let top3: u32 = totals.iter().rev().take(3).sum();
-    let top1 = *totals.last().context("missing top")?;
-
-    assert_eq!(top1, 70764);
-    assert_eq!(top3, 203905);
-    Ok(())
-}
-
-/// Parse input lines.
-fn parse() -> Result<[u32; 4]> {
     let mut input = lib::input!("d01.txt");
 
     let mut output = [0; 4];
     let mut calories = 0u32;
 
-    while let Some((n, Ws(lines))) = input.try_next::<(u32, Ws)>()? {
+    while let Some((n, Ws(lines))) = input.try_next::<(u32, _)>()? {
         calories += n;
 
         if lines == 2 {
             output[0] = std::mem::take(&mut calories);
-            output.sort();
+            output.sort_unstable();
         }
     }
 
     if calories != 0 {
         output[0] = calories;
-        output.sort();
+        output.sort_unstable();
     }
 
-    Ok(output)
+    let [_, a, b, c] = output;
+    let part1 = c;
+    let part2 = a + b + c;
+
+    assert_eq!(part1, 70764);
+    assert_eq!(part2, 203905);
+    Ok(())
 }
