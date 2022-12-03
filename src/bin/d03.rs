9879f6952d68d686cf86489c5fe89b1ecd7ba6e1
John-John Tedro <udoprog@tedro.se>
2022-12-03 06:27:34 +0100

    Use bitsets for part 1

--- src/bin/d03.rs
+++ src/bin/d03.rs
@@ -3,19 +3,14 @@ use lib::prelude::*;
 fn main() -> Result<()> {
     let mut input = lib::input!("d03.txt");
 
-    let mut total = 0;
+    let mut part1 = 0;
     let mut part2 = 0;
 
     while let Some(data) = input.try_line::<&str>()? {
-        let half = data.len() / 2;
-        let (first, second) = data.split_at(half);
-
-        for c in first.chars() {
-            if second.contains(c) {
-                total += score(c);
-                break;
-            }
-        }
+        let (first, second) = data.split_at(data.len() / 2);
+        let first = Set::from_string(first).0;
+        let second = Set::from_string(second).0;
+        part1 += (first & second).trailing_zeros();
     }
 
     input.reset();
@@ -23,12 +18,10 @@ fn main() -> Result<()> {
     while let Some(Set(a)) = input.try_line::<Set>()? {
         let Set(b) = input.line::<Set>()?;
         let Set(c) = input.line::<Set>()?;
-
-        let out = (a & b) & c;
-        part2 += out.trailing_zeros();
+        part2 += ((a & b) & c).trailing_zeros();
     }
 
-    assert_eq!(total, 8233);
+    assert_eq!(part1, 8233);
     assert_eq!(part2, 2821);
     Ok(())
 }
@@ -42,13 +35,17 @@ fn score(c: char) -> u32 {
 }
 
 lib::from_input! {
-    |v: &'static str| -> Set(u64) {
+    |v: &'static str| -> Set(u64) { Ok(Set::from_string(v)) }
+}
+
+impl Set {
+    fn from_string(string: &str) -> Self {
         let mut n = 0u64;
 
-        for c in v.chars() {
+        for c in string.chars() {
             n |= 1u64 << (score(c) as u64);
         }
 
-        Ok(Set(n))
+        Self(n)
     }
 }
