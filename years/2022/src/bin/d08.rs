c19b05c66145ee5ea62ff8cdb24772625d932256
John-John Tedro <udoprog@tedro.se>
2022-12-08 05:45:55 +0100

    Prep Day 8

---
+++ years/2022/src/bin/d08.rs
@@ -0,0 +1,11 @@
+#![allow(warnings, unused)]
+
+use lib::prelude::*;
+
+#[entry(input = "d08.txt", expect = (0, 0))]
+fn main(input: IStr) -> Result<(u64, u64)> {
+    let mut part1 = 0;
+    let mut part2 = 0;
+
+    Ok((part1, part2))
+}
