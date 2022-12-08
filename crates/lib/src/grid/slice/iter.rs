389469290becaf31ff25cce762c826dd65d2d998
John-John Tedro <udoprog@tedro.se>
2022-12-08 12:29:45 +0100

    Move Day 8 to grid helper

--- crates/lib/src/grid/slice/iter.rs
+++ crates/lib/src/grid/slice/iter.rs
@@ -2,14 +2,12 @@ use core::marker::PhantomData;
 use core::ops::Range;
 use core::ptr;
 
-use crate::grid::slice::{
-    column_index_mut, column_index_ref, row_index_mut, row_index_ref, Column, ColumnMut, Dims, Row,
-    RowMut,
-};
+use crate::grid::slice::{column_index_mut, column_index_ref, row_index_mut, row_index_ref};
+use crate::grid::slice::{Column, ColumnMut, Dims, Row, RowMut};
 
-macro_rules! rows {
-    ($name:ident $item:ident [$($m:tt)*]) => {
-        /// Iterator over rows in a slice.
+macro_rules! single {
+    ($name:ident $item:ident $dim:ident [$($m:tt)*] $(#[$($meta:meta)*])?) => {
+        /// Iterator over a slice.
         pub struct $name<'a, T> {
             data: ptr::NonNull<[T]>,
             range: Range<usize>,
@@ -22,7 +20,7 @@ macro_rules! rows {
             pub(super) fn new(data: ptr::NonNull<[T]>, dims: &'a Dims) -> Self {
                 Self {
                     data: ptr::NonNull::from(data),
-                    range: 0..dims.rows,
+                    range: 0..dims.$dim,
                     dims,
                     _marker: PhantomData,
                 }
@@ -34,66 +32,60 @@ macro_rules! rows {
 
             #[inline]
             fn next(&mut self) -> Option<Self::Item> {
-                let row = self.range.next()?;
-                Some($item::new(self.data, self.dims, row))
+                let index = self.range.next()?;
+                Some($item::new(self.data, self.dims, index))
+            }
+
+            #[inline]
+            fn nth(&mut self, n: usize) -> Option<Self::Item> {
+                let index = self.range.nth(n)?;
+                Some($item::new(self.data, self.dims, index))
+            }
+        }
+
+        impl<'a, T> DoubleEndedIterator for $name<'a, T> {
+            #[inline]
+            fn next_back(&mut self) -> Option<Self::Item> {
+                let index = self.range.next_back()?;
+                Some($item::new(self.data, self.dims, index))
+            }
+
+            #[inline]
+            fn nth_back(&mut self, n: usize) -> Option<Self::Item> {
+                let index = self.range.nth_back(n)?;
+                Some($item::new(self.data, self.dims, index))
+            }
+        }
+
+        impl<'a, T> ExactSizeIterator for $name<'a, T> {
+            #[inline]
+            fn len(&self) -> usize {
+                self.range.len()
             }
         }
     }
 }
 
-macro_rules! columns {
-    ($name:ident $item:ident [$($m:tt)*]) => {
-        /// Iterator over columns in a slice.
-        pub struct $name<'a, T> {
-            data: ptr::NonNull<[T]>,
-            range: Range<usize>,
-            dims: &'a Dims,
-            _marker: PhantomData<&'a $($m)* [T]>,
-        }
-
-        impl<'a, T> $name<'a, T> {
-            #[inline]
-            pub(super) fn new(data: ptr::NonNull<[T]>, dims: &'a Dims) -> Self {
-                Self {
-                    data: ptr::NonNull::from(data),
-                    range: 0..dims.columns,
-                    dims,
-                    _marker: PhantomData,
-                }
-            }
-        }
-
-        impl<'a, T> Iterator for $name<'a, T> {
-            type Item = $item<'a, T>;
-
-            #[inline]
-            fn next(&mut self) -> Option<Self::Item> {
-                let column = self.range.next()?;
-                Some($item::new(self.data, self.dims, column))
-            }
-        }
-    }
-}
-
-macro_rules! row_iter {
-    ($name:ident [$($m:tt)*] $fn:ident) => {
+macro_rules! matrix {
+    ($name:ident $dim:ident [$($m:tt)*] $fn:ident $(#[$($meta:meta)*])?) => {
         /// Iterator over a row.
+        $(#[$($meta)*])*
         pub struct $name<'a, T> {
             data: ptr::NonNull<[T]>,
             dims: &'a Dims,
-            row: usize,
+            current: usize,
             range: Range<usize>,
             _marker: PhantomData<&'a $($m)* [T]>,
         }
 
         impl<'a, T> $name<'a, T> {
             #[inline]
-            pub(super) fn new(data: ptr::NonNull<[T]>, dims: &'a Dims, row: usize) -> Self {
+            pub(super) fn new(data: ptr::NonNull<[T]>, dims: &'a Dims, current: usize) -> Self {
                 Self {
                     data,
                     dims,
-                    row,
-                    range: 0..dims.columns,
+                    current,
+                    range: 0..dims.$dim,
                     _marker: PhantomData,
                 }
             }
@@ -106,7 +98,7 @@ macro_rules! row_iter {
             fn next(&mut self) -> Option<Self::Item> {
                 let index = self.range.next()?;
                 // SAFETY: we know the data was initialized correctly.
-                Some(unsafe { $fn(self.data, self.dims, self.row, index) })
+                Some(unsafe { $fn(self.data, self.dims, self.current, index) })
             }
         }
 
@@ -115,53 +107,24 @@ macro_rules! row_iter {
             fn next_back(&mut self) -> Option<Self::Item> {
                 let index = self.range.next_back()?;
                 // SAFETY: we know the data was initialized correctly.
-                Some(unsafe { $fn(self.data, self.dims, self.row, index) })
+                Some(unsafe { $fn(self.data, self.dims, self.current, index) })
+            }
+        }
+
+        impl<'a, T> ExactSizeIterator for $name<'a, T> {
+            #[inline]
+            fn len(&self) -> usize {
+                self.range.len()
             }
         }
     }
 }
 
-macro_rules! column_iter {
-    ($name:ident [$($m:tt)*] $fn:ident) => {
-        /// Iterator over columns.
-        pub struct $name<'a, T> {
-            data: ptr::NonNull<[T]>,
-            dims: &'a Dims,
-            column: usize,
-            range: Range<usize>,
-            _marker: PhantomData<&'a $($m)* [T]>,
-        }
-
-        impl<'a, T> $name<'a, T> {
-            #[inline]
-            pub(super) fn new(data: ptr::NonNull<[T]>, dims: &'a Dims, column: usize) -> Self {
-                Self {
-                    data,
-                    dims,
-                    column,
-                    range: 0..dims.rows,
-                    _marker: PhantomData,
-                }
-            }
-        }
-
-        impl<'a, T> Iterator for $name<'a, T> {
-            type Item = &'a $($m)* T;
-
-            #[inline]
-            fn next(&mut self) -> Option<Self::Item> {
-                let index = self.range.next()?;
-                Some(unsafe { $fn(self.data, self.dims, self.column, index) })
-            }
-        }
-    };
-}
-
-rows!(Rows Row []);
-rows!(RowsMut RowMut [mut]);
-columns!(Columns Column []);
-columns!(ColumnsMut ColumnMut [mut]);
-column_iter!(ColumnIter [] column_index_ref);
-column_iter!(ColumnIterMut [mut] column_index_mut);
-row_iter!(RowIter [] row_index_ref);
-row_iter!(RowIterMut [mut] row_index_mut);
+single!(Rows Row rows [] #[derive(Clone)]);
+single!(RowsMut RowMut rows [mut]);
+single!(Columns Column columns [] #[derive(Clone)]);
+single!(ColumnsMut ColumnMut columns [mut]);
+matrix!(ColumnIter rows [] column_index_ref #[derive(Clone)]);
+matrix!(ColumnIterMut rows [mut] column_index_mut);
+matrix!(RowIter columns [] row_index_ref #[derive(Clone)]);
+matrix!(RowIterMut columns [mut] row_index_mut);
