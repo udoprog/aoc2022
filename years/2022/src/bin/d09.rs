2b3e05dc76d7e380b42531048bc9e5ac1f79d10e
John-John Tedro <udoprog@tedro.se>
2022-12-09 11:57:36 +0100

    Use a single array

--- years/2022/src/bin/d09.rs
+++ years/2022/src/bin/d09.rs
@@ -8,33 +8,29 @@ fn main(mut input: IStr) -> Result<(u32, u32)> {
     let mut part1 = [0u128; { CAP * (CAP / 128) }];
     let mut part2 = [0u128; { CAP * (CAP / 128) }];
 
-    let mut h = (0, 0);
-    let mut tails = [(0, 0); 10];
+    let mut knots = [(0, 0); 10];
 
-    part1.set_bit(pos_to_index(h));
-    part2.set_bit(pos_to_index(h));
+    part1.set_bit(pos_to_index(knots[0]));
+    part2.set_bit(pos_to_index(knots[0]));
 
     for line in input.iter::<Nl<(Move, usize)>>() {
         let Nl((m, b)) = line?;
 
         for _ in 0..b {
-            h.0 += m.0;
-            h.1 += m.1;
+            knots[0].0 += m.0;
+            knots[0].1 += m.1;
 
-            for n in 0..tails.len() {
-                let h = if n == 0 { h } else { tails[n - 1] };
-                let t = &mut tails[n];
+            for n in 1..knots.len() {
+                let h = knots[n - 1];
+                let t = &mut knots[n];
 
                 if let Some(n) = move_for(&h, t) {
                     *t = n;
                 }
             }
 
-            let p1 = tails[0];
-            let p2 = tails[8];
-
-            anyhow::ensure!(p1.0.abs() < MID && p1.0.abs() < MID, "oob: {p1:?}");
-
+            let [h, p1, .., p2] = knots;
+            anyhow::ensure!(h.0.abs() < MID && h.1.abs() < MID, "oob: {h:?}");
             part1.set_bit(pos_to_index(p1));
             part2.set_bit(pos_to_index(p2));
         }
