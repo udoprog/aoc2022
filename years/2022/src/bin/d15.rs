97fd4b2456b85d1a3fed286107795b1f5f26422c
John-John Tedro <udoprog@tedro.se>
2022-12-15 09:35:35 +0100

    Precompute distances

--- years/2022/src/bin/d15.rs
+++ years/2022/src/bin/d15.rs
@@ -3,25 +3,27 @@ use lib::prelude::*;
 type Line = Split2<':', ' ', (([W; 2], Point), ([W; 4], Point))>;
 
 const QUERY: i64 = 2_000_000;
+// const QUERY: i64 = 20;
 const WIDTH: i64 = 4_000_000;
+const BEACONS: usize = 40;
 
 #[entry(input = "d15.txt", expect = (4873353, 11600823139120))]
 fn main(mut input: IStr) -> Result<(i64, i64)> {
     let mut part2 = 0;
 
-    // Rows of beacons.
-    let mut rows = ArrayVec::<_, 50>::new();
     // Spans of beacons relative to current row.
-    let mut buf = ArrayVec::<_, 50>::new();
+    let mut buf = ArrayVec::<_, BEACONS>::new();
+    // Computed distances which gives an idea of the covering span of a beacon.
+    let mut computed = ArrayVec::<_, BEACONS>::new();
 
     while let Some(Split2(((_, a), (_, b)))) = input.try_line::<Line>()? {
-        rows.push((a, b));
+        computed.push((a, (b.x - a.x).abs() + (b.y - a.y).abs()));
     }
 
     let mut part1 = 0;
     let mut x = i64::MIN;
 
-    for &(s, e) in spans(QUERY, &mut buf, &rows) {
+    for &(s, e) in spans(QUERY, &mut buf, &computed) {
         part1 += (e - x.max(s)).max(0);
         x = e.max(x);
     }
@@ -29,7 +31,7 @@ fn main(mut input: IStr) -> Result<(i64, i64)> {
     for y in 0..=(WIDTH as i64) {
         let mut x = 0i64;
 
-        for &(s, e) in spans(y, &mut buf, &rows) {
+        for &(s, e) in spans(y, &mut buf, &computed) {
             if (s..=e).contains(&x) {
                 x = e + 1;
             }
@@ -49,17 +51,15 @@ fn main(mut input: IStr) -> Result<(i64, i64)> {
 fn spans<'a, const N: usize>(
     y: i64,
     buf: &'a mut ArrayVec<(i64, i64), N>,
-    rows: &[(Point, Point)],
+    computed: &[(Point, i64)],
 ) -> &'a [(i64, i64)] {
     buf.clear();
 
-    for (a, b) in rows.iter() {
-        let d0 = dist(a, &Point { x: a.x, y });
-        let d1 = dist(a, b);
+    for (a, d) in computed {
+        let w = d - (y - a.y).abs();
 
-        if d0 <= d1 {
-            let d = d1 - d0;
-            buf.push((a.x - d, a.x + d));
+        if w >= 0 {
+            buf.push((a.x - w, a.x + w));
         }
     }
 
@@ -79,9 +79,4 @@ lib::from_input! {
     |Split((Split((_, x)), Split((_, y)))): Split<',', (Coord, Coord)>| -> Point {
         Ok(Point { x, y })
     }
-}
-
-#[inline]
-fn dist(a: &Point, b: &Point) -> i64 {
-    (b.x - a.x).abs() + (b.y - a.y).abs()
 }
